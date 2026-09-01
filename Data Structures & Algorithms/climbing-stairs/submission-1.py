f79c2b71e229cc6aacbb924898aafd69eaf1a9ce
AlexChen <117213331+awxsomes@users.noreply.github.com>
2026-09-01 13:45:25 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 0:
+            return 0
+        cache = [0] * n
+
+        cache[0], cache[1] = 1, 2
+
+        for i in range(2, n):
+            cache[i] = cache[i-2] + cache[i-1]
+            print(cache)
+        return cache[n-1]
